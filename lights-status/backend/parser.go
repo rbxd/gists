eadaccd75d0d9190b6adb1dac278f72497b0722b
Nick Turskyi <gthb@nsfw.ninja>
2022-11-29 20:45:34 +0100

    changes to the UI

---
+++ lights-status/backend/parser.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "github.com/bradfitz/gomemcache/memcache"
+    "encoding/json"
+    "os"
+    "log"
+)
+
+type netHost struct {
+    SymName   string
+    Hostname  string
+}
+
+var pingEndpoints []netHost
+
+type endpointStatus struct {
+    SymName   string   `json:"name"`
+    Up        bool     `json:"up"`
+}
+
+type apiResponse struct {
+    // could be one of "on", "off", "partial", "unknown"
+    Lights    string                  `json:"lights"`
+    Time      int64                   `json:"time"`
+    Since     int64                   `json:"since"`
+    Endpoints []endpointStatus        `json:"endpoints"`
+}
+
+func getStatusMemcache(mc memcache.Client, key string) (apiResponse, error) {
+    var systemStatus apiResponse
+
+    mcVal, err := mc.Get(key)
+
+    if err != nil {
+      return nil, err
+    }
+
+    err = json.Unmarshal(mcVal.Value, &systemStatus)
+
+    if err != nil {
+        return nil, err
+    }
+
+    return systemStatus, nil
+}
+
+
+func main() {
+    // TODO get server address, key name from configuration
+    mc := memcache.New("127.0.0.1:11211")
+    mcKey := "zl34:lights"
+
+    l := log.New(os.Stdout, "[Pinger.go] ", log.Lmsgprefix | log.LstdFlags)
+
+    // get initial status from memcached
+    //      from the previous runs
+    var systemStatus apiResponse
+
+    systemStatus = getStatusMemcache(mc, mcKey)
+
+    // DEBUG
+    l.Printf("SYSTEM STATUS: ", systemStatus)
+}
